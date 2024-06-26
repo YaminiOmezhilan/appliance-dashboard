7abb26e18b95d6279d4da0e16ca489d4def47af5
YaminiPluginlive <yamini.o@pluginlive.com>
2024-06-27 00:41:53 +0530

    added golbal fontfamily

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import theme from "./theme";
+import "./index.css"; // Import global styles if needed
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
